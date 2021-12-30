321fdea73608eeee2255725839696fbb31854fb2
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-29 23:20:19 -0500

    generated maven project via spring initializr

---
+++ fmbg/src/main/java/com/passion/fmbg/FmbgApplication.java
@@ -0,0 +1,13 @@
+package com.passion.fmbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmbgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmbgApplication.class, args);
+	}
+
+}
