e7584c1a675593a70648fab343ce4224dd8231e7
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-12-31 22:49:43 -0500

    clean slate again

--- fmbg/src/main/java/com/passion/fmbg/repos/CategoriesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.passion.fmbg.repos;
-
-import com.passion.fmbg.entities.Categories;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriesRepository extends CrudRepository<Categories, String> {
-}
--- fmbg/src/main/java/com/passion/fmbg/repos/MechanicsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.passion.fmbg.repos;
-
-import com.passion.fmbg.entities.Mechanics;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MechanicsRepository extends CrudRepository<Mechanics, String> {
-}
