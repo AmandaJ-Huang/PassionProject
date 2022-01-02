6e2a6204772088110cf8b05196e1e16638443740
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2022-01-01 19:15:19 -0500

    application.properties with global api.host.url, added gamespecification

--- fmbg/src/main/java/com/passion/fmbg/controllers/GamesController.java
+++ fmbg/src/main/java/com/passion/fmbg/controllers/GamesController.java
@@ -1,28 +1,26 @@
 package com.passion.fmbg.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.passion.fmbg.entities.Games;
 import com.passion.fmbg.entities.GamesList;
+import com.passion.fmbg.specs.GameSpec;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 @RestController
+@RequestMapping("/findmegames")
 public class GamesController {
+    private String client_id = "&client_id=a5PDFkKaa5";
 
-    @GetMapping(value = "/boardgames")
-    public GamesList getGames() throws IOException {
-        String url = "https://api.boardgameatlas.com/api/search?ids=TAAifFP590&client_id=a5PDFkKaa5";
+    @Value("${api.host.url}")
+    private String bgApi;
+
+    @GetMapping(value = "&{gameSpec}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public GamesList getGames(GameSpec gameSpec) {
         RestTemplate template = new RestTemplate();
 
-        return template.getForObject(url, GamesList.class);
+        return template.getForObject(bgApi + gameSpec + client_id, GamesList.class);
     }
 }
