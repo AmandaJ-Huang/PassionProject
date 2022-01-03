e9478319f29e077bcc1381b159a0869f5aa29ddc
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2022-01-02 21:00:03 -0500

    generated button

--- angular-fmbg/src/app/components/header/header.component.ts
+++ angular-fmbg/src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'FIND ME A BOARD GAME!';
+  title: string = 'FMBG';
 
   constructor() { }
 
