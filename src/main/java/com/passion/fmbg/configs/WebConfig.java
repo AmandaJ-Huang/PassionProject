edcb7ee5aa260df928e952d0a8791df820a94dc4
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2022-01-09 23:48:26 -0500

    modified cors

--- src/main/java/com/passion/fmbg/configs/WebConfig.java
+++ src/main/java/com/passion/fmbg/configs/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://fmbg.herokuapp.com")
+            .allowedOrigins("https://fmbg.herokuapp.com", "http://localhost:4200", "http://localhost:8080")
             .allowedMethods("PUT", "DELETE", "GET", "POST");
     }
 }
