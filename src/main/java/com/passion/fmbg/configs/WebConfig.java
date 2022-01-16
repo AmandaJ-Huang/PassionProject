082b55a89d95691afad458db45023893e14bd07a
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2022-01-16 02:35:16 -0500

    updated daily log and fixed whitelabel error page again

--- src/main/java/com/passion/fmbg/configs/WebConfig.java
+++ src/main/java/com/passion/fmbg/configs/WebConfig.java
@@ -1,15 +1,18 @@
 package com.passion.fmbg.configs;
 
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     
     @Override
@@ -21,7 +24,18 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**")
-            .addResourceLocations("public/");
+        registry.addResourceHandler("/**")
+            .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+
+                        return requestedResource.exists() && requestedResource.isReadable()
+                                ? requestedResource : new ClassPathResource("/static/index.html");
+                    }
+                });
     }
 }
